40b053a4449c1be2890469784c81c615335b7b51
laxmikanta81 <laxmikantan81@gmail.com>
2026-06-18 09:47:04 +0530

    securuty improvement

--- auth.js
+++ auth.js
@@ -2,24 +2,22 @@ const passport=require('passport');
 const LocalStrategy=require('passport-local').Strategy;
 const Person=require('./models/Person');
 const bcrypt = require('bcrypt');
-passport.use(new LocalStrategy(async (Username, password, done) => {
-  try {
-    console.log('recived credential',Username,password)
-    const user = await Person.findOne({ username:Username });
-    if (!user) {
-      return done(null, false, { message: 'Incorrect username.' });
-    }
+passport.use(new LocalStrategy(
+  async (username, password, done) => {
+    try {
+      console.log('Received credentials:', username, password);
+      const user = await Person.findOne({ username });
+      if (!user) return done(null, false, { message: 'Incorrect username.' });
 
-    const isPasswordMatch =user.password === password ? true : false;
-    if (isPasswordMatch) {
-      return done(null, user);
-    } else {
-      return done(null, false, { message: 'Incorrect password.' });
+      const isPasswordMatch = await user.comparePassword(password);
+      if (isPasswordMatch) return done(null, user);
+      else return done(null, false, { message: 'Incorrect password.' });
+    } catch (error) {
+      return done(error);
     }
-  } catch (error) {
-    return done(error);
   }
-}));
+));
+
 
 
 module.exports=passport;
--- models/Person.js
+++ models/Person.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
 const bcrypt = require('bcrypt');
+const { is } = require('type-is');
 
 const personSchema=new mongoose.Schema({
     name:{
@@ -33,6 +34,26 @@ const personSchema=new mongoose.Schema({
         required:true,
     }
 });
+
+personSchema.pre('save', async function(next) {
+  const person = this;
+  if (!person.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    person.password = await bcrypt.hash(person.password, salt); // assign back
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+personSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        const isMatch=await bcrypt.compare(candidatePassword,this.password);
+        return isMatch;
+    }catch(error){
+        throw error;
+    }
+}
 
 const Person=mongoose.model('Person',personSchema);
 module.exports=Person;
